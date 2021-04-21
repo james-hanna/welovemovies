350b8406a51f236e79eb6d9979d6cc7cbf2c7d02
James Hanna <James.Hanna87@gmail.com>
2021-04-20 19:51:11 -0600

    first commit

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+require("dotenv").config();
+//old url : postgresql://postgres@localhost/postgres
+
+const {
+  DATABASE_URL = "postgres://romqrqie:tV70VF0WYeVUHJnOvs5K3-0y2EiJe3As@queenie.db.elephantsql.com:5432/romqrqie",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
